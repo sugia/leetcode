8c4c19ea79354c8ddfb0cfe236ee20a7fc106826
Shujia Liu <lausugia@gmail.com>
2015-01-14 18:27:24 +0800

    Create combination-sum.py

---
+++ combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    #given a list of integers and an integer
+    #return a list of lists of integers
+    def combinationSum(self, num, target):
+        res = []
+        self.cal(sorted(num), target, 0, [], res)
+        return res
+    def cal(self, num, target, i, cur, res):
+        if target == 0:
+            res.append(cur)
+            return
+        can = set()
+        for j in range(i, len(num)):
+            if num[j] > target:
+                return
+            if num[j] in can:
+                continue
+            can.add(num[j])
+            self.cal(num, target - num[j], j, cur + [num[j]], res)

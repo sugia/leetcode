45a2f7145427b0e18a1faa2687e35a20be7566ca
Shujia Liu <shujialiu@outlook.com>
2018-05-12 22:23:35 -0700

    Create Unique_Morse_Code_Words.py

---
+++ Unique_Morse_Code_Words.py
@@ -0,0 +1,58 @@
+'''
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+ 
+
+Note:
+
+The length of words will be at most 100.
+Each words[i] will have length in range [1, 12].
+words[i] will only consist of lowercase letters.
+
+'''
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        
+        char_to_code = {}
+        for i in xrange(26):
+            char_to_code[chr(i + ord('a'))] = codes[i]
+        
+        res = set()
+
+        for word in words:
+            res.add(self.getCode(word, char_to_code))
+            
+        return len(res)
+    
+    def getCode(self, word, char_to_code):
+        res = ''
+        for c in word:
+            res += char_to_code[c]
+        return res
+            
+

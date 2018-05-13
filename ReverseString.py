886579246c025e9e36983832d90c6535879307c1
Shujia Liu <shujialiu@outlook.com>
2018-05-13 08:56:21 -0700

    Create ReverseString.py

---
+++ ReverseString.py
@@ -0,0 +1,18 @@
+'''
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+'''
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ''
+        for c in s[::-1]:
+            res += c
+            
+        return res

7f8bd1a50e226079a6614ec406db80e172616b69
Shujia Liu <shujialiu@outlook.com>
2018-05-13 11:45:20 -0700

    Create Prime Number of Set Bits in Binary Representation.py

---
+++ Prime Number of Set Bits in Binary Representation.py
@@ -0,0 +1,67 @@
+'''
+Given two integers L and R, find the count of numbers in the range [L, R] (inclusive) having a prime number of set bits in their binary representation.
+
+(Recall that the number of set bits an integer has is the number of 1s present when written in binary. For example, 21 written in binary is 10101 which has 3 set bits. Also, 1 is not a prime.)
+
+Example 1:
+
+Input: L = 6, R = 10
+Output: 4
+Explanation:
+6 -> 110 (2 set bits, 2 is prime)
+7 -> 111 (3 set bits, 3 is prime)
+9 -> 1001 (2 set bits , 2 is prime)
+10->1010 (2 set bits , 2 is prime)
+Example 2:
+
+Input: L = 10, R = 15
+Output: 5
+Explanation:
+10 -> 1010 (2 set bits, 2 is prime)
+11 -> 1011 (3 set bits, 3 is prime)
+12 -> 1100 (2 set bits, 2 is prime)
+13 -> 1101 (3 set bits, 3 is prime)
+14 -> 1110 (3 set bits, 3 is prime)
+15 -> 1111 (4 set bits, 4 is not prime)
+Note:
+
+L, R will be integers L <= R in the range [1, 10^6].
+R - L will be at most 10000.
+'''
+
+class Solution(object):
+    def countPrimeSetBits(self, L, R):
+        """
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        
+        res = 0
+        for i in xrange(L, R+1):
+            if self.isGoodRepresentation(i):
+                res += 1
+                
+        return res
+    
+    def isGoodRepresentation(self, x):
+        tmp = self.getBitCount(x)
+        if self.isPrime(tmp):
+            return True
+        return False
+    
+    def getBitCount(self, x):
+        res = 0
+        while x > 0:
+            res += x & 1
+            x >>= 1
+        return res
+    
+    def isPrime(self, tmp):
+        if tmp < 2:
+            return False
+        for i in xrange(2, tmp - 1):
+            if tmp % i == 0:
+                return False
+            
+        return True

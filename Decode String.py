563e1176a97d7895e70fa3aa869648a534d1943a
Shujia Liu <shujialiu@outlook.com>
2018-05-17 22:07:10 -0700

    Create Decode String.py

---
+++ Decode String.py
@@ -0,0 +1,48 @@
+'''
+
+Given an encoded string, return it's decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+'''
+
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for c in s:
+            if c.isdigit():
+                tmp = int(c)
+                if len(stack) > 0 and type(stack[-1]) == type(tmp):
+                    stack[-1] = stack[-1] * 10 + tmp
+                else:
+                    stack.append(tmp)
+            elif c.isalpha():
+                stack.append(c)
+            elif c == '[':
+                stack.append('[')
+            else:
+                tmp = ''
+                while len(stack) > 0 and stack[-1] != '[':
+                    top = stack.pop()
+                    tmp = top + tmp
+                if len(stack) > 0:
+                    stack.pop()
+                num = stack.pop()
+                stack.append(tmp * num)
+                    
+        
+        
+        return ''.join(stack)

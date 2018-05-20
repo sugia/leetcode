8fa0d851b756f50e6677f1f56300a80673fbae00
Shujia Liu <shujialiu@outlook.com>
2018-05-19 20:39:41 -0700

    Create Sliding Window Maximum.py

---
+++ Sliding Window Maximum.py
@@ -0,0 +1,46 @@
+'''
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+'''
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        vec = []
+        res = []
+        for i in xrange(len(nums)):
+            while vec and nums[vec[-1]] <= nums[i]:
+                vec.pop()
+            vec.append(i)
+            
+            if i - k >= vec[0]:
+                vec.pop(0)
+                
+            if i + 1 >= k:
+                res.append(nums[vec[0]])
+                
+        return res

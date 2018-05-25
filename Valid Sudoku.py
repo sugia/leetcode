92ccc9bbc0b77caf4cfad001db51a7d8571e615f
Shujia Liu <shujialiu@outlook.com>
2018-05-24 21:01:19 -0700

    Create Valid Sudoku.py

---
+++ Valid Sudoku.py
@@ -0,0 +1,93 @@
+'''
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+'''
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in xrange(9):
+            if not self.isValid(board[i]):
+                return False
+            
+        for j in xrange(9):
+            vec = []
+            for i in xrange(9):
+                vec.append(board[i][j])
+            if not self.isValid(vec):
+                return False
+            
+        for i in xrange(0, 9, 3):
+            for j in xrange(0, 9, 3):
+                vec = []
+                for ii in xrange(i, i+3):
+                    for jj in xrange(j, j+3):
+                        vec.append(board[ii][jj])
+                if not self.isValid(vec):
+                    return False
+                
+        return True
+    
+    def isValid(self, vec):
+        fre = {}
+        for i in xrange(1, 10):
+            fre[str(i)] = 1
+            
+        for x in vec:
+            if x.isdigit():
+                if x in fre:
+                    fre[x] -= 1
+                    if fre[x] < 0:
+                        return False
+                else:
+                    return False
+        return True

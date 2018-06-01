df57e90f0b99998eee68a7bab703a9f9084dd073
Shujia Liu <shujialiu@outlook.com>
2018-05-31 17:38:09 -0700

    Create Number of 1 Bits.py

---
+++ Number of 1 Bits.py
@@ -0,0 +1,27 @@
+'''
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Example 1:
+
+Input: 11
+Output: 3
+Explanation: Integer 11 has binary representation 00000000000000000000000000001011 
+Example 2:
+
+Input: 128
+Output: 1
+Explanation: Integer 128 has binary representation 00000000000000000000000010000000
+'''
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        bit_count = 0
+        while n > 0:
+            bit_count += n & 1
+            n >>= 1
+            
+        return bit_count

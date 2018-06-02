f14e8b99b8169ae3e1dffafd77456266f76e7335
Shujia Liu <shujialiu@outlook.com>
2018-06-02 16:53:54 -0700

    Create Single Number II.py

---
+++ Single Number II.py
@@ -0,0 +1,28 @@
+'''
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+'''
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        box = set()
+        for num in nums:
+            box.add(num)
+            
+        return (sum(box) * 3 - sum(nums)) // 2

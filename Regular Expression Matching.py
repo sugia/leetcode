f3e86b617c836d950ae2e31ab33fae7d5212cbf8
Shujia Liu <shujialiu@outlook.com>
2018-06-13 22:07:27 -0700

    Create Regular Expression Matching.py

---
+++ Regular Expression Matching.py
@@ -0,0 +1,69 @@
+'''
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+Note:
+
+    s could be empty and contains only lowercase letters a-z.
+    p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+
+Example 5:
+
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false
+
+
+'''
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if not p:
+            return not s
+        
+        if len(p) > 1 and p[1] == '*':
+            return (self.isMatch(s, p[2:])) or (s != '' and (p[0] == '.' or s[0] == p[0]) and self.isMatch(s[1:], p))
+        else:
+            return s != '' and (p[0] == '.' or s[0] == p[0]) and self.isMatch(s[1:], p[1:])

f5966ac95242f8c282dfdb51f692c8c1cdd393af
Shujia Liu <shujialiu@outlook.com>
2018-06-17 13:47:18 -0700

    Create Total Hamming Distance.py

---
+++ Total Hamming Distance.py
@@ -0,0 +1,41 @@
+'''
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Now your job is to find the total Hamming distance between all pairs of the given numbers.
+
+Example:
+
+Input: 4, 14, 2
+
+Output: 6
+
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case). So the answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+
+Note:
+
+    Elements of the given array are in the range of 0 to 10^9
+    Length of the array will not exceed 10^4.
+
+'''
+
+class Solution(object):
+    def totalHammingDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        res = 0
+        for idx in xrange(32):
+            bit = 1 << idx
+            count = 0
+            
+            for num in nums:
+                if num & bit:
+                    count += 1
+                
+            res += count * (len(nums) - count)
+            
+        return res

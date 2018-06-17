e627e69e17959c0fbe0360a50852a47f65f1666e
Shujia Liu <shujialiu@outlook.com>
2018-06-17 13:52:36 -0700

    Create Brick Wall.py

---
+++ Brick Wall.py
@@ -0,0 +1,49 @@
+'''
+There is a brick wall in front of you. The wall is rectangular and has several rows of bricks. The bricks have the same height but different width. You want to draw a vertical line from the top to the bottom and cross the least bricks.
+
+The brick wall is represented by a list of rows. Each row is a list of integers representing the width of each brick in this row from left to right.
+
+If your line go through the edge of a brick, then the brick is not considered as crossed. You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
+
+You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+
+Example:
+
+Input: 
+[[1,2,2,1],
+ [3,1,2],
+ [1,3,2],
+ [2,4],
+ [3,1,2],
+ [1,3,1,1]]
+Output: 2
+Explanation: 
+
+Note:
+
+    The width sum of bricks in different rows are the same and won't exceed INT_MAX.
+    The number of bricks in each row is in range [1,10,000]. The height of wall is in range [1,10,000]. Total number of bricks of the wall won't exceed 20,000.
+
+'''
+
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        gap_count = {}
+        for row in wall:
+            gap = 0
+            for i in xrange(len(row) - 1):
+                gap += row[i]
+                if gap in gap_count:
+                    gap_count[gap] += 1
+                else:
+                    gap_count[gap] = 1
+            
+        max_count = 0
+        for gap in gap_count:
+            max_count = max(max_count, gap_count[gap])
+            
+        return len(wall) - max_count

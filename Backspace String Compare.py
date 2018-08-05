55516a5559ced08a5aad5c7266d28628e97040cd
Shujia Liu <shujialiu@outlook.com>
2018-08-05 00:13:13 -0700

    Create Backspace String Compare.py

---
+++ Backspace String Compare.py
@@ -0,0 +1,65 @@
+'''
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+Note:
+
+    1 <= S.length <= 200
+    1 <= T.length <= 200
+    S and T only contain lowercase letters and '#' characters.
+
+Follow up:
+
+    Can you solve it in O(N) time and O(1) space?
+
+
+
+'''
+
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        s = []
+        for c in S:
+            if c == '#':
+                if s:
+                    s.pop()
+            else:
+                s.append(c)
+        
+        t = []
+        for c in T:
+            if c == '#':
+                if t:
+                    t.pop()
+            else:
+                t.append(c)
+                
+        return s == t

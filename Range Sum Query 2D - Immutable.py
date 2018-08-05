a47ce3cabcc12225cadfdf3927b56dfbeb2bab67
Shujia Liu <shujialiu@outlook.com>
2018-08-04 23:04:10 -0700

    Create Range Sum Query 2D - Immutable.py

---
+++ Range Sum Query 2D - Immutable.py
@@ -0,0 +1,59 @@
+'''
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+
+Note:
+
+    You may assume that the matrix does not change.
+    There are many calls to sumRegion function.
+    You may assume that row1 ≤ row2 and col1 ≤ col2.
+
+'''
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+            self.box = [[]]
+        else:
+            self.box = [[0 for j in xrange(len(matrix[0]) + 1)] for i in xrange(len(matrix) + 1)]
+        
+        for i in xrange(1, len(matrix) + 1):
+            for j in xrange(1, len(matrix[i-1]) + 1):
+                self.box[i][j] = matrix[i-1][j-1] + self.box[i-1][j] + self.box[i][j-1] - self.box[i-1][j-1]
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        
+        return self.box[row2+1][col2+1] - self.box[row1][col2+1] - self.box[row2+1][col1] + self.box[row1][col1]
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)

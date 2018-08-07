392871d8243ee5e4d678dd7be552022ca2e14d14
Shujia Liu <shujialiu@outlook.com>
2018-08-06 19:13:38 -0700

    Create Sort Transformed Array.py

---
+++ Sort Transformed Array.py
@@ -0,0 +1,33 @@
+'''
+ Given a sorted array of integers nums and integer values a, b and c. Apply a quadratic function of the form f(x) = ax2 + bx + c to each element x in the array.
+
+The returned array must be in sorted order.
+
+Expected time complexity: O(n)
+
+Example:
+
+nums = [-4, -2, 2, 4], a = 1, b = 3, c = 5,
+
+Result: [3, 9, 15, 33]
+
+nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
+
+Result: [-23, -5, 1, 7]
+
+'''
+
+class Solution(object):
+    def sortTransformedArray(self, nums, a, b, c):
+        """
+        :type nums: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: List[int]
+        """
+        res = []
+        for x in nums:
+            res.append(a * x * x + b * x + c)
+            
+        return sorted(res)

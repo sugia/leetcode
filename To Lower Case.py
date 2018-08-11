1e8b7b499504394ccff6122eb378c9eb17771d74
Shujia Liu <shujialiu@outlook.com>
2018-08-11 16:58:50 -0700

    Create To Lower Case.py

---
+++ To Lower Case.py
@@ -0,0 +1,30 @@
+'''
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+
+Input: "here"
+Output: "here"
+
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+
+
+'''
+
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()

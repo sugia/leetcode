912976cd9735f4638b14b8d860f1f9ba0f20b630
Shujia Liu <shujialiu@outlook.com>
2018-08-14 21:21:35 -0700

    Update 4sum.py

--- 4sum.py
+++ 4sum.py
@@ -1,26 +1,49 @@
-class Solution:
-    #given a list of integers and an integer
-    #return a list of lists of integers
-    def fourSum(self, num, target):
-        num.sort()
-        map = {}
-        for i in range(len(num)):
-            for j in range(i+1, len(num)):
-                tmp = num[i] + num[j]
-                if tmp not in map:
-                    map[tmp] = [[i, j]]
-                else:
-                    map[tmp].append([i, j])
+'''
+Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+
+The solution set must not contain duplicate quadruplets.
+
+Example:
+
+Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+
+
+'''
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        dic = {}
         res = []
-        can = set()
-        for i in range(len(num)):
-            for j in range(i+1, len(num)):
-                tmp = target - num[i] - num[j]
-                if tmp in map:
-                    for x in map[tmp]:
-                        if j < x[0]:
-                            cur = [num[i], num[j], num[x[0]], num[x[1]]]
-                            if tuple(cur) not in can:
-                                can.add(tuple(cur))
-                                res.append(cur)
+        for i in xrange(len(nums)):
+            for j in xrange(i+1, len(nums)):
+                tmp = nums[i] + nums[j]
+                
+                if target - tmp in dic:
+                    for (x, y) in dic[target - tmp]:
+                        if i != x and i != y and j != x and j != y:
+                            candidate = sorted([nums[i], nums[j], nums[x], nums[y]])
+                            if candidate not in res:
+                                res.append(candidate)
+                if tmp in dic:
+                    dic[tmp].add((i, j))
+                else:
+                    dic[tmp] = set([(i, j)])
         return res
+        
+                    
+                    
+        

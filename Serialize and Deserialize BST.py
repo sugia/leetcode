e58cdd09ebebe8c6aa5ab6242e3fda18b4f6d83d
Shujia Liu <shujialiu@outlook.com>
2018-08-18 16:55:36 -0700

    Create Serialize and Deserialize BST.py

---
+++ Serialize and Deserialize BST.py
@@ -0,0 +1,60 @@
+'''
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless. 
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        vec = []
+        self.ser(root, vec)
+        return ' '.join(vec)
+
+    def ser(self, node, vec):
+        if node:
+            vec.append(str(node.val))
+            self.ser(node.left, vec)
+            self.ser(node.right, vec)
+        else:
+            vec.append('#')
+            
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        it = iter(data.split(' '))
+        return self.des(it)
+
+            
+    def des(self, it):
+        tmp = next(it)
+        if tmp == '#':
+            return None
+        else:
+            node = TreeNode(int(tmp))
+            node.left = self.des(it)
+            node.right = self.des(it)
+            return node
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))

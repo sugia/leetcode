8861ec2e42196e6dcc483dfefca8c2c42703978c
Shujia Liu <shujialiu@outlook.com>
2018-08-28 23:02:02 -0700

    Update Sort Transformed Array.py

--- Sort Transformed Array.py
+++ Sort Transformed Array.py
@@ -26,8 +26,40 @@ class Solution(object):
         :type c: int
         :rtype: List[int]
         """
-        res = []
-        for x in nums:
-            res.append(a * x * x + b * x + c)
-            
-        return sorted(res)
+        if a == 0:
+            if b == 0:
+                return [c for i in xrange(len(nums))]
+            elif b > 0:
+                return [b * x + c for x in nums]
+            else:
+                # b < 0
+                return [b * x + c for x in reversed(nums)]
+        elif a > 0:
+            mid = -b / (2.0 * a)
+            i = 0
+            j = len(nums) - 1
+            res = []
+            while i <= j:
+                if abs(nums[i] - mid) > abs(nums[j] - mid):
+                    res = [self.f(nums[i], a, b, c)] + res
+                    i += 1
+                else:
+                    res = [self.f(nums[j], a, b, c)] + res
+                    j -= 1
+            return res
+        else:
+            # a < 0
+            mid = -b / (2.0 * a)
+            i = 0
+            j = len(nums) - 1
+            res = []
+            while i <= j:
+                if (abs(nums[i] - mid) > abs(nums[j] - mid)):
+                    res.append(self.f(nums[i], a, b, c))
+                    i += 1
+                else:
+                    res.append(self.f(nums[j], a, b, c))
+                    j -= 1
+            return res
+    def f(self, x, a, b, c):
+        return a * x * x + b * x + c

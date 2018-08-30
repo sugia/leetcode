27dd908df953085600208f9e417b90694f727351
Shujia Liu <shujialiu@outlook.com>
2018-08-29 17:06:44 -0700

    Update and rename divide-two-integers.py to Divide Two Integers.py

---
+++ Divide Two Integers.py
@@ -0,0 +1,54 @@
+'''
+Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero.
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+
+Note:
+
+    Both dividend and divisor will be 32-bit signed integers.
+    The divisor will never be 0.
+    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
+
+
+'''
+
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        sign = 1
+        if dividend < 0:
+            sign = -sign
+            dividend = -dividend
+        if divisor < 0:
+            sign = -sign
+            divisor = -divisor
+            
+        res = 0
+        while dividend >= divisor:
+            tmp = divisor
+            i = 1
+            while dividend >= tmp:
+                dividend -= tmp
+                res += i
+                i <<= 1
+                tmp <<= 1
+        
+        if sign == -1:
+            res = -res
+        return min(max(res, -2147483648), 2147483647)

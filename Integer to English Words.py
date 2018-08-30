2d422f896a7ae475e140c62b726bcc3acc57383a
Shujia Liu <shujialiu@outlook.com>
2018-08-29 22:29:46 -0700

    Update Integer to English Words.py

--- Integer to English Words.py
+++ Integer to English Words.py
@@ -23,46 +23,26 @@ Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Th
 
 
 '''
-
 class Solution(object):
     def numberToWords(self, num):
         """
         :type num: int
         :rtype: str
         """
-        if not num or num == 0:
-            return 'Zero'
-        
-        num_str = {1000000000: 'Billion', 1000000: 'Million', 1000: 'Thousand', 100: 'Hundred', 90: 'Ninety', 80: 'Eighty', 70: 'Seventy', 60: 'Sixty', 50: 'Fifty', 40: 'Forty', 30: 'Thirty', 20: 'Twenty', 19: 'Nineteen', 18: 'Eighteen', 17: 'Seventeen', 16: 'Sixteen', 15: 'Fifteen', 14: 'Fourteen', 13: 'Thirteen', 12: 'Twelve', 11: 'Eleven', 10: 'Ten', 9: 'Nine', 8: 'Eight', 7: 'Seven', 6: 'Six', 5: 'Five', 4: 'Four', 3: 'Three', 2: 'Two', 1: 'One'}
+        dic = {1000000000: 'Billion', 1000000: 'Million', 1000: 'Thousand', 100: 'Hundred', 90: 'Ninety', 80: 'Eighty', 70: 'Seventy', 60: 'Sixty', 50: 'Fifty', 40: 'Forty', 30: 'Thirty', 20: 'Twenty', 19: 'Nineteen', 18: 'Eighteen', 17: 'Seventeen', 16: 'Sixteen', 15: 'Fifteen', 14: 'Fourteen', 13: 'Thirteen', 12: 'Twelve', 11: 'Eleven', 10: 'Ten', 9: 'Nine', 8: 'Eight', 7: 'Seven', 6: 'Six', 5: 'Five', 4: 'Four', 3: 'Three', 2: 'Two', 1: 'One'}
         
         res = []
-        if num >= 1000000000:
-            res.append(self.numberToWords(num // 1000000000))
-            res.append(num_str[1000000000])
-            num %= 1000000000
-            
-        if num >= 1000000:
-            res.append(self.numberToWords(num // 1000000))
-            res.append(num_str[1000000])
-            num %= 1000000
-            
-        if num >= 1000:
-            res.append(self.numberToWords(num // 1000))
-            res.append(num_str[1000])
-            num %= 1000
-            
-        if num >= 100:
-            res.append(self.numberToWords(num // 100))
-            res.append(num_str[100])
-            num %= 100
-            
-        if num >= 20:
-            k = num // 10 * 10
-            res.append(num_str[k])
-            num -= k
-            
-        if num > 0:
-            res.append(num_str[num])
-            
+        for k in sorted(dic.keys(), reverse=True):
+            if num >= k:
+                if k >= 100:
+                    res.append(self.numberToWords(num // k))
+                res.append(dic[k])
+                num %= k
+        
+        if not res:
+            return 'Zero'
         return ' '.join(res)
+            
+            
+        
             

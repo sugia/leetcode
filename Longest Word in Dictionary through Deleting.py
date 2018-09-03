7decc848b29ecafe56b62ed4401684f45289d400
Shujia Liu <shujialiu@outlook.com>
2018-09-02 18:23:04 -0700

    Create Longest Word in Dictionary through Deleting.py

---
+++ Longest Word in Dictionary through Deleting.py
@@ -0,0 +1,45 @@
+'''
+Given a string and a string dictionary, find the longest string in the dictionary that can be formed by deleting some characters of the given string. If there are more than one possible results, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
+
+Example 1:
+Input:
+s = "abpcplea", d = ["ale","apple","monkey","plea"]
+
+Output: 
+"apple"
+Example 2:
+Input:
+s = "abpcplea", d = ["a","b","c"]
+
+Output: 
+"a"
+Note:
+All the strings in the input will only contain lower-case letters.
+The size of the dictionary won't exceed 1,000.
+The length of all the strings in the input won't exceed 1,000.
+'''
+
+class Solution(object):
+    def findLongestWord(self, s, d):
+        """
+        :type s: str
+        :type d: List[str]
+        :rtype: str
+        """
+        d.sort(key = lambda x: (-len(x), x))
+        for x in d:
+            if self.valid(x, s):
+                return x
+        return ''
+    
+    def valid(self, x, s):
+        i = 0
+        j = 0
+        while i < len(x):
+            while j < len(s) and s[j] != x[i]:
+                j += 1
+            if j >= len(s):
+                return False
+            j += 1
+            i += 1
+        return i >= len(x)

3552976ade7f1e34bd7fa25cf7a8d78465f2999c
Shujia Liu <shujialiu@outlook.com>
2018-09-18 21:52:22 -0700

    Create Department Top Three Salaries.py

---
+++ Department Top Three Salaries.py
@@ -0,0 +1,43 @@
+'''
+The Employee table holds all employees. Every employee has an Id, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who earn the top three salaries in each of the department. For the above tables, your SQL query should return the following rows.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+'''
+
+# Write your MySQL query statement below
+select d1.Name as Department, e1.Name as Employee, e1.Salary
+from Employee e1
+join Department d1
+on e1.DepartmentId = d1.Id
+where 3 > (select count(distinct(e2.Salary)) 
+           from Employee e2
+           where e2.Salary > e1.Salary and e2.DepartmentId = e1.DepartmentId)
+order by Department, e1.Salary desc

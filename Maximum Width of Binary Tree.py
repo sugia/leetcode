d28ee190fcd47dc8c53e85bbd79309d2a3a7b8e2
Shujia Liu <shujialiu@outlook.com>
2018-09-22 14:48:11 -0700

    Create Maximum Width of Binary Tree.py

---
+++ Maximum Width of Binary Tree.py
@@ -0,0 +1,90 @@
+'''
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        dic = {}
+        self.find(root, dic, 1, 1)
+        
+        res = 0
+        for k, v in dic.iteritems():
+            left = min(v)
+            right = max(v)
+            res = max(res, right - left + 1)
+        return res
+    
+    def find(self, node, dic, level, w):
+        if level in dic:
+            dic[level].append(w)
+        else:
+            dic[level] = [w]
+        
+        if node.left:
+            self.find(node.left, dic, level+1, w*2-1)
+        if node.right:
+            self.find(node.right, dic, level+1, w*2)

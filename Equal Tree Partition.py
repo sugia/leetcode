4af73ec2ffeb50578b90288663d5ee81617614de
Shujia Liu <shujialiu@outlook.com>
2018-09-22 14:37:33 -0700

    Create Equal Tree Partition.py

---
+++ Equal Tree Partition.py
@@ -0,0 +1,66 @@
+'''
+Given a binary tree with n nodes, your task is to check if it's possible to partition the tree to two trees which have the equal sum of values after removing exactly one edge on the original tree.
+
+Example 1:
+Input:     
+    5
+   / \
+  10 10
+    /  \
+   2   3
+
+Output: True
+Explanation: 
+    5
+   / 
+  10
+      
+Sum: 15
+
+   10
+  /  \
+ 2    3
+
+Sum: 15
+Example 2:
+Input:     
+    1
+   / \
+  2  10
+    /  \
+   2   20
+
+Output: False
+Explanation: You can't split the tree into two trees with equal sum after removing exactly one edge on the tree.
+Note:
+The range of tree node value is in the range of [-100000, 100000].
+1 <= n <= 10000
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def checkEqualTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        dic = {}
+        self.find(root, dic)
+        for node in dic:
+            if node != root and dic[node] * 2 == dic[root]:
+                return True
+        return False
+    
+    def find(self, node, dic):
+        if not node:
+            return 0
+        left = self.find(node.left, dic)
+        right = self.find(node.right, dic)
+        dic[node] = left + right + node.val
+        return dic[node]

fea84581aa8fc4a8c5e9a75072d95a48eb47e1c2
Shujia Liu <shujialiu@outlook.com>
2018-09-27 14:53:00 -0700

    Update Basic Calculator II.py

--- Basic Calculator II.py
+++ Basic Calculator II.py
@@ -32,4 +32,44 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return eval(s)
+        s = s.replace(' ', '')
+        return self.cal(s, 0)[0]
+    
+    def update(self, stack, op, tmp):
+        if op == '+':
+            stack.append(tmp)
+        elif op == '-':
+            stack.append(-tmp)
+        elif op == '*':
+            stack[-1] = stack[-1] * tmp
+        elif op == '/':
+            if stack[-1] >= 0:
+                stack[-1] //= tmp
+            else:
+                stack[-1] = -(-stack[-1] // tmp)
+        
+    def cal(self, s, idx):
+        '''
+        s: str
+        idx: int
+        rtype: (sum, idx)
+        '''
+        stack = []
+        op = '+'
+        tmp = 0
+        while idx < len(s):
+            if s[idx].isdigit():
+                tmp = tmp * 10 + int(s[idx])
+                idx += 1
+            elif s[idx] == '(':
+                tmp, idx = self.cal(s, idx+1)
+            elif s[idx] == ')':
+                self.update(stack, op, tmp)
+                return sum(stack), idx+1
+            else:
+                self.update(stack, op, tmp)
+                op = s[idx]
+                tmp = 0
+                idx += 1
+        self.update(stack, op, tmp)
+        return sum(stack), idx

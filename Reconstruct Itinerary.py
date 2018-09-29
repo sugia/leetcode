81e4803a543fd67113f814df5583e91a5838693d
Shujia Liu <shujialiu@outlook.com>
2018-09-29 03:18:51 -0700

    Create Reconstruct Itinerary.py

---
+++ Reconstruct Itinerary.py
@@ -0,0 +1,42 @@
+'''
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+
+Note:
+
+If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+Example 1:
+
+Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+Example 2:
+
+Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+             But it is larger in lexical order.
+'''
+
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        dic = {}
+        for a, b in reversed(sorted(tickets)):
+            if a in dic:
+                dic[a].append(b)
+            else:
+                dic[a] = [b]
+        
+        res = []
+        self.find('JFK', dic, res)
+        return res[::-1]
+    
+    def find(self, x, dic, res):
+        while x in dic and dic[x]:
+            y = dic[x].pop()
+            self.find(y, dic, res)
+        res.append(x)

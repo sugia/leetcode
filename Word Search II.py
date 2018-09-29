a990fb8c1b4c9fd49f3ec90de81c07c76705aac3
Shujia Liu <shujialiu@outlook.com>
2018-09-29 02:53:54 -0700

    Update Word Search II.py

--- Word Search II.py
+++ Word Search II.py
@@ -20,47 +20,30 @@ Note:
 You may assume that all inputs are consist of lowercase letters a-z.
 '''
 
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word = None
-        
 class Solution(object):
-    def findWords(self, board, words):
+    def wordPatternMatch(self, pattern, str):
         """
-        :type board: List[List[str]]
-        :type words: List[str]
-        :rtype: List[str]
+        :type pattern: str
+        :type str: str
+        :rtype: bool
         """
-        root = TrieNode()
         
-        for word in words:
-            node = root
-            for c in word:
-                if c not in node.children:
-                    node.children[c] = TrieNode()
-                node = node.children[c]
-            node.word = word
-        
-        res = set()
-        for i in xrange(len(board)):
-            for j in xrange(len(board[i])):
-                if board[i][j] in root.children:
-                    visited = [[False for jj in xrange(len(board[i]))] for ii in xrange(len(board))]
-                    visited[i][j] = True
-                    self.find(board, visited, i, j, root.children[board[i][j]], res)
-                
-        return list(res)
+        dic = {}
+        return self.find(pattern, str, dic)
     
-    def find(self, board, visited, x, y, node, res):
-        if node.word:
-            res.add(node.word)
-            
-        for (i, j) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            if 0 <= x + i < len(board) and 0 <= y + j < len(board[x+i]):
-                if not visited[x+i][y+j] and board[x+i][y+j] in node.children:
-                    visited[x+i][y+j] = True
-                    self.find(board, visited, x+i, y+j, node.children[board[x+i][y+j]], res)
-                    visited[x+i][y+j] = False
+    def find(self, pattern, str, dic):
+        if not pattern:
+            return not str
         
+        for i in xrange(1, len(str)+1):
+            if pattern[0] in dic and dic[pattern[0]] == str[:i]:
+                if self.find(pattern[1:], str[i:], dic):
+                    return True
+            if pattern[0] not in dic and str[:i] not in dic.values():
+                dic[pattern[0]] = str[:i]
+                if self.find(pattern[1:], str[i:], dic):
+                    return True
+                del dic[pattern[0]]
+        return False
+            
         

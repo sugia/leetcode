c66545c76937dbbc57f7619eaa1bdba94741f0fb
Shujia Liu <shujialiu@outlook.com>
2019-03-16 14:17:07 -0700

    Create Verifying an Alien Dictionary.py

---
+++ Verifying an Alien Dictionary.py
@@ -0,0 +1,53 @@
+'''
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Note:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are english lowercase letters.
+'''
+
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        dic = {}
+        for i in xrange(len(order)):
+            dic[order[i]] = i
+        
+        for i in xrange(len(words)):
+            for j in xrange(i+1, len(words)):
+                if len(words[i]) > len(words[j]) and words[i].startswith(words[j]):
+                    return False
+                for k in xrange(min(len(words[i]), len(words[j]))):
+                    if words[i][k] != words[j][k]:
+                        if dic[words[i][k]] > dic[words[j][k]]:
+                            return False
+                        break
+        return True

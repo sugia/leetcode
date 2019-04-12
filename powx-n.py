9bbc1a88ae56fee54e7b2913e68a5259c91fcebb
Shujia Liu <shujialiu@outlook.com>
2019-04-12 11:43:05 -0700

    Update powx-n.py

--- powx-n.py
+++ powx-n.py
@@ -1,19 +1,42 @@
-class Solution:
-    #given two doubles
-    #return one double
-    def pow(self, x, n):
-        #return x ** n
-        if n < 0:
-            return 1.0 / self.cal(x, -n)
-        else:
-            return self.cal(x, n)
-    def cal(self, x, n):
+'''
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+
+'''
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
         if n == 0:
             return 1
-        elif n == 1:
-            return x
-        tmp = self.cal(x, n >> 1)
+        elif n < 0:
+            n = -n
+            x = 1.0 / x
+        
         if n & 1:
-            return tmp * tmp * x
-        else:
-            return tmp * tmp
+            return self.myPow(x, n // 2) ** 2 * x
+        
+        return self.myPow(x, n // 2) ** 2
+        

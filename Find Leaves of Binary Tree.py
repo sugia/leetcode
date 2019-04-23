82991ee933c3fe9e74692048763c4a60d312178f
Shujia Liu <shujialiu@outlook.com>
2019-04-22 19:29:43 -0700

    Update Find Leaves of Binary Tree.py

--- Find Leaves of Binary Tree.py
+++ Find Leaves of Binary Tree.py
@@ -35,51 +35,26 @@ Special thanks to @elmirap for adding this problem and creating all test cases.
 '''
 
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def findLeaves(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def findLeaves(self, root: TreeNode) -> List[List[int]]:
         res = []
-        if not root:
-            return res
-        
-        dic = {}
-        self.build(root, dic)
-        
-        for key in xrange(max(dic.keys())+1):
-            res.append(dic[key])
+        self.find(root, res)
         return res
-        
-    def build(self, node, dic):
+    
+    def find(self, node: TreeNode, res: List[List[int]]) -> int:
         if not node:
             return -1
         
-        if node.left:
-            left_high = self.build(node.left, dic)
-            if node.right:
-                right_high = self.build(node.right, dic)
-                
-                high = max(left_high, right_high) + 1
-            else:
-                high = left_high + 1
-        else:
-            if node.right:
-                right_high = self.build(node.right, dic)
-                high = right_high + 1
-            else:
-                high = 0
-                
-        if high in dic:
-            dic[high].append(node.val)
-        else:
-            dic[high] = [node.val]
-                
-        return high
+        level = 1 + max(self.find(node.left, res), self.find(node.right, res))
+        
+        if level >= len(res):
+            res.append([])
+        res[level].append(node.val)
+        return level
+        
